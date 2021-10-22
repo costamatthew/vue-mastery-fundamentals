520687ea8c6a431e28ce3c2b29ae9be83da44a1f
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-10-22 18:11:27 -0300

    Multiple Vue Instances

---
+++ app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+  data() {
+    return {
+      firstName: "Mateus",
+      lastName: "Costa",
+    };
+  },
+}).mount("#app");
+
+// Vue.createApp({
+//   data() {
+//     return {
+//       firstName: "Matheus",
+//       lastName: "Costa",
+//     };
+//   },
+// }).mount("#app2");
